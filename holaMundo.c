3a8dda5e3f1803082af32f855434382b2a851a1c
g4ab0o <2172040810@correo.ler.uam.mx>
2021-08-10 20:09:09 -0500

    Programa de prueba en C
    Programa hola mundo para probar compilador

---
+++ holaMundo.c
@@ -0,0 +1,11 @@
+/*Descripción:
+* Este es un programa de prueba para probar el compilador 
+Nombre:   Gabriel Ulises Lagunez Medina 
+Fecha:    10 / 08 / 2021
+
+*/
+# include <stdio.h>
+int main (){
+    printf("hola mundo de la UAM \n");
+    return 0;
+}
