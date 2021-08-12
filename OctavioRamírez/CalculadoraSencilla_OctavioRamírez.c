2be43928f5d389fefee89c29e44fd48966b94c0c
Lunaticals <al2132001731@azc.uam.mx>
2021-08-12 13:08:41 -0500

    Calculadora Sencilla añadida.

---
+++ OctavioRamírez/CalculadoraSencilla_OctavioRamírez.c
@@ -0,0 +1,127 @@
+//PENDIENTE DE DOCUMENTAR
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool seleccionarOpcion(){
+
+    bool continuar = false;
+    int entrada;
+    scanf("%d", &entrada);
+    float primer, segundo; 
+    char respuesta;
+
+    switch(entrada){
+        case 1:
+            printf("\nProporcione el primer número que se va a sumar: ");
+            scanf("%f", &primer);
+
+            printf("Proporcione el segundo número que se va a sumar: ");
+            scanf("%f", &segundo);
+
+            printf("\nLa suma de los valores proporcionados es: %f\n", primer+segundo);
+
+            printf("\n¿Desea continuar utilizando la calculadora? (S/N): ");
+            
+            scanf("%s", &respuesta);
+            
+            if(respuesta == 's' || respuesta == 'S'){
+                continuar = true;
+            }else{
+                printf("\nGracias por utilizar ORR-CALC, hasta pronto.\n\n\n");
+            }
+            
+        break;
+            
+        case 2:
+            printf("\nProporcione el número del cual se va a restar: ");
+            scanf("%f", &primer);
+            
+            printf("Proporcione el número que se va a restar: ");
+            scanf("%f", &segundo);
+
+            printf("\nLa resta de los valores proporcionados es: %f\n", primer-segundo);
+            
+            printf("\n¿Desea continuar utilizando la calculadora? (S/N): ");
+            
+            scanf("%s", &respuesta);
+            if(respuesta == 's' || respuesta == 'S'){
+                continuar = true;
+            }else{
+                printf("\nGracias por utilizar ORR-CALC, hasta pronto.\n\n\n");
+            }
+
+        break;
+        
+        case 3:
+            printf("\nProporcione el primer número a multiplicar: ");
+            scanf("%f", &primer);
+
+            printf("Proporcione el segundo número a multiplicar: ");
+            scanf("%f", &segundo);
+            
+            printf("El producto de los valores proporcionados es: %f\n", primer*segundo);
+
+            printf("\n¿Desea continuar utilizando la calculadora? (S/N): ");
+            
+            scanf("%s", &respuesta);
+            if(respuesta == 's' || respuesta == 'S'){
+                continuar = true;
+            }else{
+                printf("\nGracias por utilizar ORR-CALC, hasta pronto.\n\n\n");
+            }
+
+        break;
+        
+        case 4:
+            printf("\nProporcione el número que desea dividir: ");
+            scanf("%f", &primer);
+
+            printf("Proporcione el número entre el cual se dividirá: ");
+            scanf("%f", &segundo);
+            
+            printf("\nLa división de los valores proporcionados es: %f\n", primer/segundo);
+
+            printf("\n¿Desea continuar utilizando la calculadora? (S/N): ");
+            
+            scanf("%s", &respuesta);
+            if(respuesta == 's' || respuesta == 'S'){
+                continuar = true;
+            }else{
+                printf("\nGracias por utilizar ORR-CALC, hasta pronto.\n\n\n");
+            }
+
+        break;
+        
+        case 5:
+            printf("\nGracias por utilizar ORR-CALC, hasta pronto.\n\n\n");
+        
+        break;
+        
+        
+        default:
+            printf("\nEl valor seleccionado no corresponde a ninguna de las opciones disponibles.\n");
+            printf("Inténtelo nuevamente.\n\n");
+
+        break;
+    }
+    return continuar;
+}
+
+void imprimirMenu(){
+    do{
+        printf("\nCalculadora simple.\n");
+        printf("1. Sumar.\n");
+        printf("2. Restar.\n");
+        printf("3. Multiplicar.\n");
+        printf("4. Dividir.\n");
+        printf("5. Salir.\n");
+        printf("\nSeleccione el número con la operación que desea realizar: ");
+    }while(seleccionarOpcion()==true);
+}
+
+
+int main(){
+    imprimirMenu();
+}
